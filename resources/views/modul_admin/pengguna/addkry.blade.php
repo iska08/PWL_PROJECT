097779f3b96e33413685b8d773b5a139a5055d9c
iska08 <aamuhaimin08@gmail.com>
2022-06-29 10:33:06 +0700

    as'ad

--- resources/views/modul_admin/pengguna/addkry.blade.php
+++ resources/views/modul_admin/pengguna/addkry.blade.php
@@ -5,7 +5,7 @@
 <div class="col-md-12 col-12">
     <div class="card">
         <div class="card-header">
-            <h4 class="card-title">Form Tambah Data Admin</h4>
+            <h4 class="card-title">Form Tambah Data Admin Cabang</h4>
         </div>
         <div class="card-content">
             <div class="card-body">
